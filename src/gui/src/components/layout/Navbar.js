883afffc2e4ee6092baafd9ca113aca3d6cef744
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2020-12-25 22:54:16 +0530

    Navbar: Added 3 new buttons and a new icon to the navbar.
    This commit introduces 3 buttons, Home, about, Support and a github icon.

--- src/gui/src/components/layout/Navbar.js
+++ src/gui/src/components/layout/Navbar.js
@@ -1,13 +1,54 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
-const Navbar = () =>{
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  root: {
+    flex: 1,
+  },
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+
   return (
-    <AppBar position="absolute">
-      <Toolbar>
+    <AppBar position="absolute" className={classes.root}>
+      <Toolbar variant="dense">
+        <Button
+          variant="contained"
+          color="primary"
+          fontSize="large"
+          disableElevation
+          className={classes.margin}
+        >
+          Home
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          fontSize="large"
+          disableElevation
+          className={classes.margin}
+        >
+          About
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          fontSize="large"
+          disableElevation
+          className={classes.margin}
+        >
+          Support
+        </Button>
+        <GitHubIcon fontSize="large" />
       </Toolbar>
     </AppBar>
-  )
-}
+  );
+};
 
 export default Navbar;
